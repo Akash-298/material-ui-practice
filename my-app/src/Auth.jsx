6cf2f0abb93b73114fe445196171d7befc0952ca
Akash-298 <akash.c.c298@gmail.com>
2023-10-12 08:17:30 +0530

    doing signup component

---
+++ my-app/src/Auth.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Typography, Box,TextField } from '@mui/material';
+
+function Auth() {
+  return (
+         <form>
+            <Box display='flex' >
+                <Typography>Hii</Typography>
+               <TextField></TextField>
+               <TextField></TextField>
+               <TextField></TextField>
+            </Box>
+        </form>
+        )
+}
+
+export default Auth
